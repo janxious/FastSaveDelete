c8f067893fd3cb82a5e952f6939420c334a23e77
Joel Meador <joel@expectedbehavior.com>
2018-05-24 11:03:43 -0400

    initial release

---
+++ FastSaveDelete.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reflection;
+using BattleTech.Save.SaveGameStructure;
+using BattleTech.UI;
+using Harmony;
+using UnityEngine;
+
+namespace FastSaveDelete
+{
+    public class FastSaveDelete
+    {
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("com.joelmeador.FastSaveDelete");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(SGLoadSavedGameScreen), "DeleteSave")]
+    public static class SGLoadSavedGameScreenDeleteSavePatch
+    {
+        static bool Prefix(
+            ref SlotModel slot,
+            SGLoadSavedGameScreen __instance)
+        {
+//  The code that is commented would work, except we can't use
+//  the slot parameter in an anonymous function so we are forced
+//  to not have safety for latest save.
+//            SlotModel mostRecentSave = __instance.saveStructure.MostRecentSave;
+//            if (slot == mostRecentSave)
+//            {
+//                var bringToFrontMethodInfo =
+//                    ReflectionHelper.GetPrivateMethodInfo(__instance, "BringToFront", new Type[] { });
+//                var action = (Action) Delegate.CreateDelegate(typeof(Action), bringToFrontMethodInfo);
+//                
+//                GenericPopupBuilder.Create("Delete Save", "Are you sure?")
+//                    .AddButton("Cancel", action, true, null).AddButton("OK", delegate
+//                        {
+//                            ActuallyDeleteSave(__instance, slot);
+//                        }, true, null).IsNestedPopupWithBuiltInFader().Render();
+//            }
+//            else
+//            {
+            ActuallyDeleteSave(__instance, slot);
+//            }
+
+            return false;
+        }
+
+        private static void ActuallyDeleteSave(SGLoadSavedGameScreen screen, SlotModel slot)
+        {
+            try
+            {
+                screen.saveStructure.Delete(slot);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Exception caught: {ex.Message}");
+            }
+
+            ReflectionHelper.InvokePrivateMethode(screen, "BringToFront", new object[] { }, new Type[] { });
+        }
+    }
+
+    public static class ReflectionHelper
+    {
+        public static MethodInfo GetPrivateMethodInfo(object instance, string methodname, Type[] types)
+        {
+            Type type = instance.GetType();
+            MethodInfo methodInfo = type.GetMethod(methodname, BindingFlags.NonPublic | BindingFlags.Instance, null,
+                types, null);
+            return methodInfo;
+        }
+
+        public static object InvokePrivateMethode(object instance, string methodname, object[] parameters, Type[] types)
+        {
+            MethodInfo methodInfo = GetPrivateMethodInfo(instance, methodname, types);
+            return methodInfo.Invoke(instance, parameters);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Fast Save Delete
+
+BattleTech Mod (using [BTML](https://github.com/Mpstark/BattleTechModLoader) and [ModTek](https://github.com/Mpstark/ModTek)).
+
+## Features
+* No comfirmation prompt for deleting save games
+
+## Download
+Downloads can be found on [Github](https://github.com/janxious/FastSaveDelete/releases).
+
+## Install
+- [Install BTML and Modtek](https://github.com/Mpstark/ModTek/wiki/The-Drop-Dead-Simple-Guide-to-Installing-BTML-&-ModTek-&-ModTek-mods).
+- Put the `FastSaveDelete.dll` and `mod.json` files into `\BATTLETECH\Mods\FastSaveDelete` folder.
+- If you want to change the settings do so in the mod.json.
+- Start the game.
+
+## Special Thanks
+
+HBS, @Mpstark, @Morphyum
+
+## Maintainer Notes: New HBS Patch Instructions
+
+* pop open VS
+* grab the latest version of the assembly
+* copy the new version of the methods in `original_src` over the existing ones
+* see if anything important changed via git
